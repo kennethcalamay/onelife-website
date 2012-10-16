087feb1bb0f4163443d71882a588201387c42ff2
Kenneth Calamay <dev+kenneth@proudcloud.net>
2012-10-16 23:13:43 +0800

    Added links to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 OnelifeWebsite::Application.routes.draw do
+  get '/about', :to => "pages#about"
+  get '/photos', :to => "pages#photos"
+  get '/videos', :to => "pages#videos"
+  get '/contact_us', :to => "pages#contact_us"
+  get '/donate', :to => "pages#donate"
+
   root :to => "pages#index"
 end
